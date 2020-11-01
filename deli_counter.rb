9b1c7b93f13f388292d8ec67e7e7d2bbd223ede2
Nathan Glick <nateleighglick@hotmail.com>
2020-11-01 10:38:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,10 +16,10 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
-def now_serving(katz_deli, name)
-  if
+def now_serving(katz_deli)
+  if katz_deli
     puts "Currently serving #{name}"
-  else
+  else katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   end 
 end
