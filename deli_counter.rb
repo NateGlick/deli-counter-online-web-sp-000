9d86e540a20e491709c9d8854d4cd5cc970f7de3
Nathan Glick <nateleighglick@hotmail.com>
2020-11-01 10:34:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
-def now_serving()
+def now_serving(katz_deli, name)
+  if
+    puts "Currently serving #{name}"
+  else
+    puts
